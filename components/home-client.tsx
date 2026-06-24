e70f399cc8f6d6a0a0144dda7d2b00f603d367a8
ojackson27 <ollie.jackson27@gmail.com>
2026-06-24 17:59:25 +0100

    Wire new editorial sections into homepage, remove video hero and dark carousel

--- components/home-client.tsx
+++ components/home-client.tsx
@@ -1,15 +1,25 @@
 import EditorialNav from "@/components/editorial-nav";
 import EditorialFooter from "@/components/editorial-footer";
-import HeroMediaPlane from "@/components/hero-media-plane";
-import PortfolioCarousel from "@/components/portfolio-carousel";
+import HomeHero from "@/components/home-hero";
+import HomeProblem from "@/components/home-problem";
+import HomeDifferentiators from "@/components/home-differentiators";
+import HomeWork from "@/components/home-work";
+import HomeProcess from "@/components/home-process";
+import HomeTestimonials from "@/components/home-testimonials";
+import HomeFinalCta from "@/components/home-final-cta";
 
 export default function HomeClient() {
   return (
     <div className="bg-white">
       <EditorialNav />
 
-      <HeroMediaPlane />
-      <PortfolioCarousel />
+      <HomeHero />
+      <HomeProblem />
+      <HomeDifferentiators />
+      <HomeWork />
+      <HomeProcess />
+      <HomeTestimonials />
+      <HomeFinalCta />
 
       <EditorialFooter />
     </div>
