efba813084f2304f86546480ecdb78eb70b05028
ojackson27 <ollie.jackson27@gmail.com>
2026-06-29 14:28:12 +0100

    remove: testimonials section from homepage

--- components/home-client.tsx
+++ components/home-client.tsx
@@ -5,7 +5,6 @@ import HomeProblem from "@/components/home-problem";
 import HomeDifferentiators from "@/components/home-differentiators";
 import HomeWork from "@/components/home-work";
 import HomeProcess from "@/components/home-process";
-import HomeTestimonials from "@/components/home-testimonials";
 import ScrollRiver from "@/components/scroll-river";
 import MobileStickyCta from "@/components/mobile-sticky-cta";
 import HomeLoadingScreen from "@/components/home-loading-screen";
@@ -22,7 +21,6 @@ export default function HomeClient() {
       <HomeDifferentiators />
       <HomeWork />
       <HomeProcess />
-      <HomeTestimonials />
       <EditorialFooter />
     </div>
   );
